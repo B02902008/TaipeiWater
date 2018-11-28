e5b84eec3387c601ec952336323ac6c6e2cca077
陳泓文 <r07922037@ntu.edu.tw>
2018-11-28 21:55:55 +0800

    login api test

--- app/module_database/db_operation.py
+++ app/module_database/db_operation.py
@@ -5,13 +5,16 @@ def db_connect():
     try:
         db = pymysql.connect("localhost", "TaipeiWaterServer", "tpewater123", "TaipeiWater")
         cursor = db.cursor()
-    except pymysql.err.OperationalError:
+    except pymysql.MySQLError:
         return null, null
     return db, cursor
 
 
 def sql_execute(cursor, sql):
-    cursor.execute(sql)
+    try:
+        cursor.execute(sql)
+    except pymysql.MySQLError:
+        return ()
     return cursor.fetchall()
 
 
--- app/module_user/view.py
+++ app/module_user/view.py
@@ -1,19 +1,28 @@
 from flask import request
 from . import blue_user
 from app.module_database import db_operation as db_op
+import uuid
+import json
 
 
 @blue_user.route('/login', methods=['POST'])
 def login():
     db, cursor = db_op.db_connect()
+    if db == null or cursor == null:
+        return {"success": -1, "msg": "資料庫錯誤"}
     sql = "SELECT id, help FROM users WHERE username='" + \
           request.values['username'] + "' AND password=Password('" + request.values['password'] + "')"
     result = db_op.sql_execute(cursor, sql)
+    if len(result) != 1:
+        db_op.db_close(db)
+        return {"success": -1, "msg": "資料庫錯誤"}
+    uid = result[0][0]
+    help = (result[0][1] == 1)
+    token = str(uuid.uuid1())
+    sql = "UPDATE users SET token='" + token + "', tokenExpire=DATE_ADD(NOW(), INTERVAL 3 DAY) WHERE id=" + uid
+    db_op.sql_execute(cursor, sql)
     db_op.db_close(db)
-    print(result)
-    print(type(result))
-    print(len(result))
-    return 'Hi'
+    return {"success": 1, "msg": json.dumps({"uid": uid, "help": help, "token": token})}
 
 
 @blue_user.route('/token', methods=['POST'])
