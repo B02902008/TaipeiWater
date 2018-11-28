8bbd18a71d825bc55439c9d1cbc86a78d25cf552
陳泓文 <r07922037@ntu.edu.tw>
2018-11-28 14:29:16 +0800

    structure adjust

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask import render_template
 from app.module_load import blue_load
+from app.module_user import blue_user
 
 app = Flask(__name__)
 app.register_blueprint(blue_load, url_prefix='/load')
+app.register_blueprint(blue_user, url_prefix='/user')
 
 
 @app.route('/')
