184f489a2434ceec48d5c2eafaeb948ac6174c5d
陳泓文 <r07922037@ntu.edu.tw>
2018-11-29 16:46:06 +0800

    register api test

--- app/static/js/sidebar.js
+++ app/static/js/sidebar.js
@@ -7,6 +7,10 @@ $(document).ready(function () {
         toggle_sidebar();
     });
     $('#menu-show_all').addClass("open");
+    $('.menu-list-item').click(function () {
+        $('.menu-list-item').removeClass("open");
+        this.addClass("open");
+    })
 });
 function toggle_sidebar() {
     $('#sidebar-overlay').toggle();
