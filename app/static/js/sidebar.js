438d3c02a8ed9614908564e3a5715b15cbd1aaa4
陳泓文 <r07922037@ntu.edu.tw>
2018-11-29 16:48:53 +0800

    sidebar css test

--- app/static/js/sidebar.js
+++ app/static/js/sidebar.js
@@ -6,10 +6,10 @@ $(document).ready(function () {
     $('#sidebar-overlay').click(function () {
         toggle_sidebar();
     });
-    $('#menu-show_all').addClass("open");
+    $('#menu-show_all').addClass("menu-selected");
     $('.menu-list-item').click(function () {
-        $('.menu-list-item').removeClass("open");
-        this.addClass("open");
+        $('.menu-list-item').removeClass("menu-selected");
+        this.addClass("menu-selected");
     })
 });
 function toggle_sidebar() {
