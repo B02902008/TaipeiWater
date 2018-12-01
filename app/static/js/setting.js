f4b606e958feb20ff0c7d27e76fa0c4cce96da32
陳泓文 <r07922037@ntu.edu.tw>
2018-12-01 14:26:10 +0800

    setting js test

--- app/static/js/setting.js
+++ app/static/js/setting.js
@@ -2,9 +2,10 @@ $(document).ready(function () {
     let view_type_arr = window.view_type.split("").map(function(x){return(x==="1");});
     let view_status_arr = window.view_status.split("").map(function(x){return(x==="1");});
     let view_range_val = window.view_range;
-    let tmp = true;
+    let tmp;
+    tmp = true;
     view_type_arr.forEach(function (val, idx) {
-        $('input:checkbox[name=place][value='+ idx +']').prop("checked", val);
+        $('input:checkbox[name=place][value=' + idx + ']').prop("checked", val);
         tmp = tmp & val;
     });
     $('input:checkbox[name=place-all]').prop("checked", tmp);
@@ -14,5 +15,23 @@ $(document).ready(function () {
     $('input:checkbox[name=place]').click(function () {
         $('input:checkbox[name=place-all]').prop("checked",
             $("input:checkbox:checked[name=place]").length === view_type_arr.length);
+    });
+    if (!window.help) {
+        $('input:checkbox[name=status][value=1]').prop("disabled", true);
+        $('input:checkbox[name=status][value=2]').prop("disabled", true);
+    }
+    tmp = true;
+    view_status_arr.forEach(function (val, idx) {
+        $('input:checkbox[name=status][value='+ idx +']').prop("checked", val);
+        tmp = tmp & val;
+    });
+    $('input:checkbox[name=status-all]').prop("checked", tmp);
+
+    $('input:checkbox[name=status-all]').click(function() {
+        $('input:checkbox[name=status]').prop("checked", $('input:checkbox[name=status-all]').prop("checked"));
+    });
+    $('input:checkbox[name=status]').click(function () {
+        $('input:checkbox[name=status-all]').prop("checked",
+            $("input:checkbox:checked[name=status]").length === view_status_arr.length);
     });
 });
