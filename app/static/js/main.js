a6e77a6e9365fe44bfb0cd211f8d563090b61574
陳泓文 <r07922037@ntu.edu.tw>
2018-12-02 18:14:22 +0800

    get data api test

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -132,7 +132,7 @@ function distance(p1, p2) {
     let radLat1 = p1.lat * Math.PI / 180.0;
     let radLat2 = p2.lat * Math.PI / 180.0;
     let a = radLat1 - radLat2;
-    let  b = p1.lng * Math.PI / 180.0 - p2 * Math.PI / 180.0;
+    let b = p1.lng * Math.PI / 180.0 - p2.lng * Math.PI / 180.0;
     let s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) +
         Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(b / 2), 2)));
     s = s * 6378.137;
